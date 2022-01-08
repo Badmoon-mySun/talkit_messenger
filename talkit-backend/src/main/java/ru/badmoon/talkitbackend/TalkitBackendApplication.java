254a405e92aed4dd4b29e59da1628c9072068aac
Anvar <anvar00755@mail.ru>
2022-01-08 17:38:15 +0300

    initial

---
+++ talkit-backend/src/main/java/ru/badmoon/talkitbackend/TalkitBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.badmoon.talkitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkitBackendApplication.class, args);
+	}
+
+}
---
+++ talkit-backend/src/test/java/ru/badmoon/talkitbackend/TalkitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.badmoon.talkitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
